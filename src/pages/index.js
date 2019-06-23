a0f4110ebc39fe1172ed80085b0581351bcffe3b
Arnau Siches <asiches@gmail.com>
2019-06-23 17:10:44 +0100

    feat: Show recents
    Signed-off-by: Arnau Siches <asiches@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,67 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
+import { graphql, Link } from 'gatsby'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
 import Meta from '../components/meta'
 
-function IndexPage() {
+function Index({data}) {
+  const set = data.allMarkdownRemark.edges
+
   return (
     <Layout>
       <Meta title="Home" />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-        <Image />
-      </div>
-      <Link to="/page-2/">Go to page 2</Link>
+      <h1>Recent</h1>
+      {
+        set.map(({node}) => {
+          return (
+            <div key={node.frontmatter.id}>
+              <h2><Link
+                to={node.fields.slug}>{node.headings[0].value}</Link></h2>
+              <span> ({node.frontmatter.date.slice(0, 10)})</span>
+              <div dangerouslySetInnerHTML={{ __html:
+                node.excerpt.replace(/^[^\n]+\n/, '') }} />
+            </div>
+          )
+        })
+      }
     </Layout>
   )
 }
 
-export default IndexPage
+Index.propTypes = {
+  data: PropTypes.object,
+  pageContext: PropTypes.object,
+}
+
+
+/* eslint no-undef: "off" */
+export const query = graphql`
+  query Recent {
+    allMarkdownRemark(
+      limit: 10
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+            group
+          }
+          timeToRead
+          excerpt(format: HTML)
+          frontmatter {
+            id
+            type
+            tags
+            date
+          }
+          headings(depth: h1) {
+            value
+          }
+        }
+      }
+    }
+  }
+`
+export default Index
