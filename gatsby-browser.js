047a88f056c45af07aa38576732a564df743762c
Arnau Siches <asiches@gmail.com>
2021-07-20 08:09:36 +0100

    chore: Remove Gatsby
    Signed-off-by: Arnau Siches <asiches@gmail.com>

--- gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-window.panelbear = window.panelbear || function() { (window.panelbear.q = window.panelbear.q || []).push(arguments) }
-panelbear('config', { site: 'HtfkzMZfG3Z' })
