e58c0337b13bdddbb5bc780623075bc022c74c90
Arnau Siches <asiches@gmail.com>
2021-08-29 14:46:25 +0100

    chore: Remove old aquarium
    Signed-off-by: Arnau Siches <asiches@gmail.com>

--- aquarium/src/cli/mod.rs
+++
@@ -1,8 +0,0 @@
-// Copyright 2020 Arnau Siches
-
-// Licensed under the MIT license <LICENCE or http://opensource.org/licenses/MIT>.
-// This file may not be copied, modified, or distributed except
-// according to those terms.
-
-pub mod bulletin;
-pub mod logo;
