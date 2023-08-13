9bccc0c2adba8ce46abca477f3d253b38b8d9dcc
Arnau Siches <asiches@gmail.com>
2023-08-13 11:25:18 +0100

    feat: Handle submission result
    Signed-off-by: Arnau Siches <asiches@gmail.com>

--- website/static/main.js
+++ website/static/main.js
@@ -1,88 +1,31 @@
-// Mailerlite
+function handleSubscriptionSuccess(submitter) {
+  submitter.textContent = "Sent";
+  submitter.disabled = true;
+  document.querySelector("#bulletin-email").disabled = true;
+}
 
-// function mailerlite() {
-//   const code = 'q2f6f6'
-//   const action = `https://app.mailerlite.com/webforms/submit/${code}`
-//   const [status, setStatus] = useState('ready')
-//   const [value, setValue] = useState('')
-//   const handleSubmit = (event) => {
-//     event.preventDefault()
-//     console.log('hi')
-//     setStatus('sending')
+function handleSubsciptionError(form) {
+  const errMsg = document.createElement("p");
+  errMsg.textContent = "There seems to be an issue. Please try later.";
+  errMsg.classList.add("error-message");
+  form.append(errMsg);
+}
 
-//     fetch(action, {
-//       method: 'POST',
-//       mode: 'cors',
-//       credentials: 'omit',
-//       referrerPolicy: 'no-referrer',
-//       headers: {
-//         'Content-Type': 'application/x-www-form-urlencoded',
-//       },
-//       body: `fields[email]=${encodeURIComponent(value)}&ml-submit=1`
-//     })
-//       .then(() => {
-//         setStatus('sent')
-//       })
-//       .catch((error) => {
-//         console.log(error)
-//         setStatus('error')
-//       })
-//   }
+window.addEventListener("load", () => {
 
-//   const classes = useStyles()
+  const bulletinForm = document.querySelector("#bulletin-form");
 
-//   if (status == 'ready') {
-//     return (
-//       <Form
-//         code={code}
-//         action={action}
-//         classes={classes}
-//         onSubmit={handleSubmit}
-//         value={value}
-//         setValue={setValue}
-//       />
-//     )
-//   }
+  if (bulletinForm) {
+    bulletinForm.addEventListener("submit", (event) => {
+      event.preventDefault();
 
-//   if (status == 'sending') {
-//     return (
-//       <React.Fragment>
-//         <Typography component="p" paragraph={true}>
-//           Sending subscription to <a href="https://www.mailerlite.com">Mailerlite</a>.
-//         </Typography>
-//         <LinearProgress color="secondary" />
-//       </React.Fragment>
-//     )
-//   }
+      const submitter = event.submitter;
+      const action = submitter.form.action;
+      const data = new FormData(submitter.form);
 
-//   if (status == 'sent') {
-//     return (
-//       <React.Fragment>
-//         <Typography component="p" paragraph={true}>
-//         Thank you for subscribing to the bulletin.
-//         </Typography>
-//         <LinearProgress value={100} variant="determinate" />
-//       </React.Fragment>
-//     )
-//   }
-
-//   if (status == 'error') {
-//     return (
-//       <React.Fragment>
-//         <Form
-//           code={code}
-//           action={action}
-//           classes={classes}
-//           onSubmit={handleSubmit}
-//           value={value}
-//           setValue={setValue}
-//         />
-//         <SnackbarContent
-//           variant="outlined"
-//           className={classes.error}
-//           message="Something went wrong. Try again later." />
-//       </React.Fragment>
-//     )
-//   }
-//   // <img src="https://track.mailerlite.com/webforms/o/1715020/q2f6f6?v4a60e9ef938a7fa0240ac9ba567062cb" width="1" height="1" style="max-width:1px;max-height:1px;visibility:hidden;padding:0;margin:0;display:block" alt="." border="0" />
-// }
+      fetch(url, { method: "POST", body: data })
+        .then(_ => handleSubscriptionSuccess(submitter))
+        .catch(_ => handleSubscriptionError(submitter.form));
+    });
+  }
+});
